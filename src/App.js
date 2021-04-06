d4dd2d25ab511a35843a62e070703198fabfe6b1
JeonMinJae <ssi02014@naver.com>
2021-04-06 17:15:14 +0900

    Add(minjae): Layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import {View, StyleSheet, Text, Button} from 'react-native';
-
-//chapter 1
-// import Counter from './components/chapter1/Counter';
-// import EventButton from './components/chapter1/EventButton';
-// import EventInput from './components/chapter1/EventInput';
-// import MyButton from './components/chapter1/MyButton';
-
-//chapter 2
-
+import { Header, Contents, Footer } from './components/chapter2/Layout';
 
 const App = () => {
     const styles = StyleSheet.create({
@@ -34,28 +26,9 @@ const App = () => {
     return (
         <>
             <View style={styles.container}>
-
-                {/* 
-                    chapter 1
-
-                    <Text style={styles.title}>
-                        {(() => {
-                            if(name === 'Minjae') return 'My name is Minjae';
-                            else return 'My First React Native';
-                        })()}
-                    </Text>
-                    <MyButton title="Minjae Button" onPress={() => alert('props')}></MyButton>
-                    <MyButton title="Minjae Button" onPress={() => alert('children')}>Children Props</MyButton>
-                    <Counter />
-                    <EventButton />
-                    <EventInput /> 
-                
-                */}
-
-                <Text style={[styles.text, {color: 'green'}]}>Class Style-Text</Text>
-                <Text style={[styles.text, styles.error]}>Class Style-Error</Text>
-
-
+                <Header></Header>
+                <Contents></Contents>
+                <Footer></Footer>
             </View>
         </>
     );
--- src/components/chapter2/Layout.js
+++ src/components/chapter2/Layout.js
@@ -1,11 +1,54 @@
 import React from 'react';
+import { StyleSheet, View, Text} from 'react-native';
 
-const Layout = () => {
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 80,
+    },
+
+    header: {
+        backgroundColor: '#f1c40f',
+        flex: 1,
+    },
+    
+    contents: {
+        backgroundColor: '#1abc9c',
+        height: 500,
+        flex: 5,
+    },
+
+    footer: {
+        backgroundColor: '#3498db',
+        flex: 1,
+    },
+    text: {
+        fontSize: 26,
+    }
+})
+
+export const Header = () => {
     return (
-        <div>
-            
-        </div>
-    );
-};
+        <View style={[styles.container, styles.header]}>
+            <Text style={styles.text}>Header</Text>
+        </View>
+    )
+}
 
-export default Layout;
+export const Contents = () => {
+    return (
+        <View style={[styles.container, styles.contents]}>
+            <Text style={styles.text}>Contents</Text>
+        </View>
+    )
+}
+
+export const Footer = () => {
+    return (
+        <View style={[styles.container, styles.footer]}>
+            <Text style={styles.text}>Footer</Text>
+        </View>
+    )
+}
